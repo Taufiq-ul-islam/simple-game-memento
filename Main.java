be7c16816cca74cfd5e6dbf46773cae71dcb67fd
Taufiq-ul-islam <taufiqulislam216@gmail.com>
2026-04-14 15:25:43 +0530

    Fixed loaded games not able to be played, list index errors

--- Main.java
+++ Main.java
@@ -56,12 +56,51 @@ public class Main {
                     if (manager.listSaves()) {
                         System.out.print("Enter save index: ");
                         int idx = sc.nextInt();
+                        try {
                         GameState state = manager.getSave(idx);
                         if (state != null) {
                             game.restore(state);
                         }
+                        while (true) {
+                            System.out.println("\n1. Damage   2. Save   3. Status   4. Exit");
+                            System.out.print("Choose: ");
+                            int subchoice = sc.nextInt();
+                            switch (subchoice) {
+                                case 1:
+                                    boolean alive = game.playTurn();
+                                    if (!alive) {
+                                        System.out.println("\n--- GAME OVER ---");
+                                        System.out.println("Exiting Game...");
+                                        flag = 1;
+                                    }
+                                    break;
+
+                                case 2:
+                                    manager.addSave(game.save());
+                                    break;
+
+                                case 3:
+                                    game.showStatus();
+                                    break;
+
+                                case 4:
+                                    flag = 1;
+                                    break;
+
+                                default:
+                                    System.out.println("Invalid choice");
+                            }
+                            if (flag == 1) {
+                                flag = 0;
+                                break;
+                            }
+                        }
                     } else
                         System.out.println("\n--- NO SAVED GAMES FOUND ---");
+                    }
+                    catch (Exception e) {
+                        System.out.println("\n--- NO SAVED GAMES FOUND ---");
+                    }
                     break;
 
                 case 3:
